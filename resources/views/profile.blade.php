275cdbb49a8bde887ade862547baa074e7895120
Phattanun Aukkarapuntouch <phattanun.auk@gmail.com>
2016-08-21 01:23:18 +0700

    move profile nav to sidebar

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,9 @@
 @extends('masterpage')
 
+@section('profileNav')
+    active
+@endsection
+
 @section('title')
     ข้อมูลส่วนตัว
 @endsection
--- resources/views/sidebar/patient.blade.php
+++ resources/views/sidebar/patient.blade.php
@@ -1,16 +1,22 @@
-<li class="nav-item">
+<li class="nav-item @yield('profileNav')">
+    <a href="{{url('/profile')}}" class="nav-link nav-toggle">
+        <i class="icon-user"></i>
+        <span class="title">ขัอมูลส่วนตัว</span>
+    </a>
+</li>
+<li class="nav-item @yield('newAppNav')">
     <a href="javascript:;" class="nav-link nav-toggle">
         <i class="icon-plus"></i>
         <span class="title">นัดหมายใหม่</span>
     </a>
 </li>
-<li class="nav-item   start active open">
+<li class="nav-item @yield('futureAppNav')">
     <a href="javascript:;" class="nav-link nav-toggle">
         <i class="icon-calendar"></i>
         <span class="title">การนัดหมายในอนาคต</span>
     </a>
 </li>
-<li class="nav-item  ">
+<li class="nav-item  @yield('pastAppNav')">
     <a href="javascript:;" class="nav-link nav-toggle">
         <i class="icon-clock"></i>
         <span class="title">ประวัติการนัดหมายในอดีต</span>
