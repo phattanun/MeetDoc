c4bf1d3b093944875f6a9e34e8b907ff4fe4cacd
Mongkud Klungpornkun <mongkud167@gmail.com>
2016-10-09 03:23:58 +0700

    Get WorkingTime for Doctor

--- app/Http/Controllers/WorkingTimeController.php
+++ app/Http/Controllers/WorkingTimeController.php
@@ -5,11 +5,112 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\DateDim;
+use App\WorkingTime;
 use App\NormalWorkingTime;
 use App\SpecialWorkingTime;
 
 class WorkingTimeController extends Controller
 {
+    private function printTable($array) {
+        if(sizeof($array) == 0) {
+            echo "<h4>Empty Table</h4>";
+            return;
+        }
+        echo "<table border='1'>";
+        echo "<tr>";
+        foreach ($array[0] as $key => $value)
+            echo "<th>".$key."</th>";
+        echo "</tr>";
+        foreach ($array as $instance) {
+            echo "<tr>";
+            foreach($instance as $key => $value)
+                echo "<td>".$value."</td>";
+            echo "</tr>";
+        }
+        echo "</table><br>";
+    }
+
+    private function printWeeklyTable($array) {
+        $dow = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        $time = ['Morning', 'Afternoon'];
+        function charToTime($c, $time) {
+            return $c == 'M' ? $time[0] : $time[1];
+        }
+        foreach ($array as $record) {
+            $table[$record['day']][charToTime($record['time'], $time)] = $record['dept_id'];
+        }
+        echo "<table border='1' style='text-align:center'><tr><th>Weekly</th>";
+        foreach ($dow as $day)
+            echo "<th>".$day."</th>";
+        echo "</tr>";
+        foreach ($time as $t) {
+            echo "<tr><th>".$t."</th>";
+            foreach ($dow as $day) {
+                echo "<td>".(isset($table[$day][$t]) ? $table[$day][$t] : "")."</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table><br>";
+    }
+
+    private function printCalendarTable($array) {
+        $dow = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        $time = ['Morning', 'Afternoon'];
+        $date = new \DateTime("@".strtotime($array[0]['date']));
+        $date->modify('last Sunday');
+        $table[$date->format('Y-m-d')]['day'] = $date->format('j');
+        $table[$date->format('Y-m-d')]['month'] = $date->format('M');
+        $table[$date->format('Y-m-d')]['dow'] = $date->format('l');
+        $table[$date->format('Y-m-d')]['free'] = True;
+        foreach ($array as $record) {
+            while($date->getTimeStamp() != strtotime($record['date'])) {
+                $date->modify("+1 day");
+                $table[$date->format('Y-m-d')]['day'] = $date->format('j');
+                $table[$date->format('Y-m-d')]['month'] = $date->format('M');
+                $table[$date->format('Y-m-d')]['dow'] = $date->format('l');
+                $table[$date->format('Y-m-d')]['free'] = True;
+            }
+            $table[$date->format('Y-m-d')]['free'] = False;
+            $table[$date->format('Y-m-d')][charToTime($record['time'],$time)] = $record['dept_id'];
+        }
+        while($date->format('l') != "Saturday") {
+            $date->modify("+1 day");
+            $table[$date->format('Y-m-d')]['day'] = $date->format('j');
+            $table[$date->format('Y-m-d')]['month'] = $date->format('M');
+            $table[$date->format('Y-m-d')]['dow'] = $date->format('l');
+            $table[$date->format('Y-m-d')]['free'] = True;
+        }
+
+        echo "<table border='1' style='text-align:center'><tr><th colspan='22'>Calendar</th></tr>";
+
+        echo "<tr><td></td>";
+        foreach ($dow as $day)
+            echo "<th colspan='3'>".$day."</th>";
+        echo "</tr>";
+
+        echo "<tr><th>Month</th>";
+        foreach ($dow as $day)
+            echo "<th>Day</th><th>เช้า</th><th>บ่าย</th>";
+        echo "</tr>";
+
+        foreach ($table as $record) {
+            if($record['dow'] == "Sunday")
+                echo "<tr><th>".$record['month']."</th>";
+            echo "<th colspan='1' style='font-size:1px'>".$record['day']."</th>";
+            if($record['free'])
+                echo "<td colspan='2' width='80px'>-</td>";
+            else {
+                foreach ($time as $t)
+                    if(isset($record[$t]))
+                        echo "<td width='40px'>".$record[$t]."</td>";
+                    else
+                        echo "<td width='40px'>-</td>";
+            }
+            if($record['dow'] == "Saturday")
+                echo "</tr>";
+        }
+        echo "</table><br>";
+    }
 
     // ############################
     //     Normal Working Time
@@ -30,25 +131,11 @@ class WorkingTimeController extends Controller
         return $array;
     }
 
-    private function tempPrintTable($array) {
-        echo "<table border='1'>";
-        echo "<tr>";
-        foreach ($array[0] as $key => $value)
-            echo "<th>".$key."</th>";
-        echo "</tr>";
-        foreach ($array as $instance) {
-            echo "<tr>";
-            foreach($instance as $key => $value)
-                echo "<td>".$value."</td>";
-            echo "</tr>";
-        }
-        echo "</table><br>";
-    }
     public function getNormalWorkingTime() {
         $normal_working_time = NormalWorkingTime::all()->toArray();
         try {
             $normal_working_time = $this->sortArrayByDayTimeAttr($normal_working_time);
-            $this->tempPrintTable($normal_working_time);
+            $this->printTable($normal_working_time);
         }
         catch (\Exception $e) {
             echo "<h2>Error: ".$e->getMessage()."</h2>";
@@ -104,8 +191,8 @@ class WorkingTimeController extends Controller
 
     private function sortArrayByDateTimeAttr($array) {
         $compare = function($a, $b) {
-            $day_a = date('w', strtotime($a['date']));
-            $day_b = date('w', strtotime($b['date']));
+            $day_a = strtotime($a['date']);
+            $day_b = strtotime($b['date']);
             $time_a = $a['time'];
             $time_b = $b['time'];
             if($day_a == $day_b) {
@@ -121,7 +208,7 @@ class WorkingTimeController extends Controller
         $special_working_time = SpecialWorkingTime::all()->toArray();
         try {
             $special_working_time = $this->sortArrayByDateTimeAttr($special_working_time);
-            $this->tempPrintTable($special_working_time);
+            $this->printTable($special_working_time);
         }
         catch (\Exception $e) {
             echo "<h2>Error: ".$e->getMessage()."</h2>";
@@ -180,16 +267,23 @@ class WorkingTimeController extends Controller
     // ############################
 
     public function getWorkingTime(Request $request) {
+        var_dump($request->all());
+
         $normalTime = NormalWorkingTime::where('doctor_ssn', $request->doctor_ssn)->get()->toArray();
         $addTime = SpecialWorkingTime::where('doctor_ssn', $request->doctor_ssn)->where('type', 'add')->get()->toArray();
         $subTime = SpecialWorkingTime::where('doctor_ssn', $request->doctor_ssn)->where('type', 'sub')->get()->toArray();
 
-        $normalTime = $this->sortArrayByDayTimeAttr($normalTime);
-        $addTime = $this->sortArrayByDateTimeAttr($addTime);
-        $subTime = $this->sortArrayByDateTimeAttr($subTime);
+        // $addTime = $this->sortArrayByDateTimeAttr($addTime);
+        // $subTime = $this->sortArrayByDateTimeAttr($subTime);
 
-        $this->tempPrintTable($normalTime);
-        $this->tempPrintTable($addTime);
-        $this->tempPrintTable($subTime);
+        $this->printWeeklyTable($normalTime);
+        echo "<h2>Special Working Time (Add)</h2>";
+        $this->printTable($addTime);
+        echo "<h2>Special Working Time (Subtract)</h2>";
+        $this->printTable($subTime);
+
+        $working_time = WorkingTime::where('doctor_ssn', $request->doctor_ssn)->where('date','>=',$request->from)->where('date','<=',$request->to)->get()->toArray();
+        $working_time = $this->sortArrayByDateTimeAttr($working_time);
+        $this->printCalendarTable($working_time);
     }
 }
