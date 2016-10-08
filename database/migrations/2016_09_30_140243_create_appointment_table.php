1bb76e4f6c6046ac0d4fa5e284912999ebe9710c
Patipol Chiammunchit <patipol.chiammunchit@telecom-bretagne.eu>
2016-10-09 00:10:15 +0700

    Appointment API: edit .sql file and checkin_time nullable

--- database/migrations/2016_09_30_140243_create_appointment_table.php
+++ database/migrations/2016_09_30_140243_create_appointment_table.php
@@ -18,7 +18,7 @@ class CreateAppointmentTable extends Migration
             $table->char('time', 1);
             $table->string('symptom');
             $table->string('queue_status');
-            $table->timestamp('checkin_time');
+            $table->timestamp('checkin_time')->nullable();
             $table->char('type', 1);
 
             $table->bigInteger('doctor_ssn')->unsigned();
