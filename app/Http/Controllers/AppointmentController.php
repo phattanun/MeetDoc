06c6b198223f37b389c9c2c840931fd8d9b3183b
Patipol Chiammunchit <patipol.chiammunchit@telecom-bretagne.eu>
2016-10-08 23:46:55 +0700

    Appointment API: fix table

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Appointment;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\App;
+use Mockery\CountValidator\Exception;
 
 class AppointmentController extends Controller
 {
-    //
+    public function create_appointment(Request $request) {
+
+        try{
+            $appointment = new Appointment();
+            $appointment->date = $request->date;
+            $appointment->time = $request->time;
+            $appointment->symptom = $request->symptom;
+            $appointment->patient_ssn = $request->patient_ssn;
+            // How can I identify the doctor_ssn if patient select clinic instead of doctor.
+            $appointment->save();
+        }
+        catch (Exception $e){
+            echo "<h2>Error</h2>";
+        }
+
+    }
+
+    public function edit_appointment(Request $request) {
+
+        try{
+            $appointment = Appointment::findOrFail($request->id);
+            $editable_key = ['date','time','']
+        }
+    }
 }
