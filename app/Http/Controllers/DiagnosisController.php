3fe1cad97a3952ee07cb9878993540a6ed72771e
Krit <ac127.44264@gmail.com>
2016-10-18 01:26:20 +0700

    no message

--- app/Http/Controllers/DiagnosisController.php
+++ app/Http/Controllers/DiagnosisController.php
@@ -67,7 +67,7 @@ class DiagnosisController extends Controller
             array_push($diagnosis_info, json_decode($appointment, true));
         }
 
-        return json_encode($diagnosis_info);
+        return $diagnosis_info;
     }
 
     public function add_given_medicine(Request $request)
