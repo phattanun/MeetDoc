1b9dd710c50ce2f9cc27eb18d461b60a504f5085
Phattanun Aukkarapuntouch <phattanun.auk@gmail.com>
2016-10-17 11:41:38 +0700

    add validation js for disease, delete unused function

---
+++ public/assets/pages/scripts/disease-form-validation.js
@@ -0,0 +1,336 @@
+var FormValidation = function () {
+
+    // basic validation
+    var handleValidation1 = function() {
+        // for more info visit the official plugin documentation:
+            // http://docs.jquery.com/Plugins/Validation
+
+            var form1 = $('#drug-add-form');
+            var error1 = $('.alert-danger', form1);
+            var success1 = $('.alert-success', form1);
+
+            form1.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "",  // validate all fields including form hidden input
+                messages: {
+                    select_multi: {
+                        maxlength: jQuery.validator.format("Max {0} items allowed for selection"),
+                        minlength: jQuery.validator.format("At least {0} items must be selected")
+                    }
+                },
+                rules: {
+                    name: {
+                        minlength: 2,
+                        required: true
+                    },
+                    input_group: {
+                        email: true,
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    url: {
+                        required: true,
+                        url: true
+                    },
+                    number: {
+                        required: true,
+                        number: true
+                    },
+                    digits: {
+                        required: true,
+                        digits: true
+                    },
+                    creditcard: {
+                        required: true,
+                        creditcard: true
+                    },
+                    occupation: {
+                        minlength: 5,
+                    },
+                    select: {
+                        required: true
+                    },
+                    select_multi: {
+                        required: true,
+                        minlength: 1,
+                        maxlength: 3
+                    }
+                },
+
+                invalidHandler: function (event, validator) { //display error alert on form submit
+                    success1.hide();
+                    error1.show();
+                },
+
+                errorPlacement: function (error, element) { // render error placement for each input type
+                    var cont = $(element).parent('.input-group');
+                    if (cont) {
+                        cont.after(error);
+                    } else {
+                        element.after(error);
+                    }
+                },
+
+                highlight: function (element) { // hightlight error inputs
+
+                    $(element)
+                        .closest('.form-group').addClass('has-error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .closest('.form-group').removeClass('has-error'); // set success class to the control group
+                },
+
+                submitHandler: function (form) {
+                    success1.show();
+                    error1.hide();
+                }
+            });
+
+
+    }
+  // basic validation
+    var handleValidation2 = function() {
+        // for more info visit the official plugin documentation:
+            // http://docs.jquery.com/Plugins/Validation
+
+            var form1 = $('#drug-edit-form');
+            var error1 = $('.alert-danger', form1);
+            var success1 = $('.alert-success', form1);
+
+            form1.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "",  // validate all fields including form hidden input
+                messages: {
+                    select_multi: {
+                        maxlength: jQuery.validator.format("Max {0} items allowed for selection"),
+                        minlength: jQuery.validator.format("At least {0} items must be selected")
+                    }
+                },
+                rules: {
+                    name: {
+                        minlength: 2,
+                        required: true
+                    },
+                    input_group: {
+                        email: true,
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    url: {
+                        required: true,
+                        url: true
+                    },
+                    number: {
+                        required: true,
+                        number: true
+                    },
+                    digits: {
+                        required: true,
+                        digits: true
+                    },
+                    creditcard: {
+                        required: true,
+                        creditcard: true
+                    },
+                    occupation: {
+                        minlength: 5,
+                    },
+                    select: {
+                        required: true
+                    },
+                    select_multi: {
+                        required: true,
+                        minlength: 1,
+                        maxlength: 3
+                    }
+                },
+
+                invalidHandler: function (event, validator) { //display error alert on form submit
+                    success1.hide();
+                    error1.show();
+                },
+
+                errorPlacement: function (error, element) { // render error placement for each input type
+                    var cont = $(element).parent('.input-group');
+                    if (cont) {
+                        cont.after(error);
+                    } else {
+                        element.after(error);
+                    }
+                },
+
+                highlight: function (element) { // hightlight error inputs
+
+                    $(element)
+                        .closest('.form-group').addClass('has-error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .closest('.form-group').removeClass('has-error'); // set success class to the control group
+                },
+
+                submitHandler: function (form) {
+                    success1.show();
+                    error1.hide();
+                }
+            });
+
+
+    }// basic validation
+    var handleValidation3 = function() {
+        // for more info visit the official plugin documentation:
+            // http://docs.jquery.com/Plugins/Validation
+
+            var form1 = $('#drug-search-form');
+            var error1 = $('.alert-danger', form1);
+            var success1 = $('.alert-success', form1);
+
+            form1.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "",  // validate all fields including form hidden input
+                messages: {
+                    select_multi: {
+                        maxlength: jQuery.validator.format("Max {0} items allowed for selection"),
+                        minlength: jQuery.validator.format("At least {0} items must be selected")
+                    }
+                },
+                rules: {
+                    name: {
+                        minlength: 2,
+                        required: true
+                    },
+                    input_group: {
+                        email: true,
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    url: {
+                        required: true,
+                        url: true
+                    },
+                    number: {
+                        required: true,
+                        number: true
+                    },
+                    digits: {
+                        required: true,
+                        digits: true
+                    },
+                    creditcard: {
+                        required: true,
+                        creditcard: true
+                    },
+                    occupation: {
+                        minlength: 5,
+                    },
+                    select: {
+                        required: true
+                    },
+                    select_multi: {
+                        required: true,
+                        minlength: 1,
+                        maxlength: 3
+                    }
+                },
+
+                invalidHandler: function (event, validator) { //display error alert on form submit
+                    success1.hide();
+                    error1.show();
+                },
+
+                errorPlacement: function (error, element) { // render error placement for each input type
+                    var cont = $(element).parent('.input-group');
+                    if (cont) {
+                        cont.after(error);
+                    } else {
+                        element.after(error);
+                    }
+                },
+
+                highlight: function (element) { // hightlight error inputs
+
+                    $(element)
+                        .closest('.form-group').addClass('has-error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .closest('.form-group').removeClass('has-error'); // set success class to the control group
+                },
+
+                submitHandler: function (form) {
+                    success1.show();
+                    error1.hide();
+                }
+            });
+
+
+    }
+
+
+    // advance validation
+
+    var handleWysihtml5 = function() {
+        if (!jQuery().wysihtml5) {
+            
+            return;
+        }
+
+        if ($('.wysihtml5').size() > 0) {
+            $('.wysihtml5').wysihtml5({
+                "stylesheets": ["../assets/global/plugins/bootstrap-wysihtml5/wysiwyg-color.css"]
+            });
+        }
+    }
+
+    return {
+        //main function to initiate the module
+        init: function () {
+
+            handleWysihtml5();
+            handleValidation1();
+            handleValidation2();
+            handleValidation3();
+
+        }
+
+    };
+
+}();
+
+jQuery(document).ready(function() {
+    FormValidation.init();
+});
