21b51698baa0a5dd39f7715ad659a19a70fc9ea5
Krit <ac127.44264@gmail.com>
2016-11-20 14:27:49 +0700

    change word in register form page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -150,7 +150,7 @@
 <script src="{{url('/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
 <script src="{{url('/assets/global/plugins/jquery.blockui.min.js')}}" type="text/javascript"></script>
 <script src="{{url('/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js')}}" type="text/javascript"></script>
-<script src="{{url('/assets/global/plugins/jquery-validation/js/jquery.validate.min.js')}}" type="text/javascript"></script>
+<script src="{{url('/assets/global/scripts/jquery.validate.register.js')}}" type="text/javascript"></script>
 <script src="{{url('/assets/global/plugins/jquery-validation/js/additional-methods.min.js')}}" type="text/javascript"></script>
 <script src="{{url('/assets/global/plugins/select2/js/select2.full.min.js')}}" type="text/javascript"></script>
 <script src="{{url('/assets/global/scripts/app.min.js')}}" type="text/javascript"></script>
