187e68ce08b90564e6335ab6531c178f120af646
Phattanun Aukkarapuntouch <phattanun.auk@gmail.com>
2016-11-20 17:40:00 +0700

    Redirect to homepage if route doesnt exist

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,6 +47,28 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        if($this->isHttpException($e))
+        {
+            switch ($e->getStatusCode())
+            {
+                // not found
+                case 404:
+                    return redirect('/');
+                    break;
+
+                // internal error
+                case '500':
+                    return redirect('/');
+                    break;
+
+                default:
+                    return $this->renderHttpException($e);
+                    break;
+            }
+        }
+        else
+            {
+                return parent::render($request, $e);
+            }
     }
 }
